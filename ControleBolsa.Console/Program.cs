6de1de00d73b614ed0425c7adb3e5d5db60fe4a6
Glaudson Silva <glaudsonsilva@gmail.com>
2016-03-09 17:44:23 -0300

    Adding relative path to database file

--- ControleBolsa.Console/Program.cs
+++ ControleBolsa.Console/Program.cs
@@ -1,7 +1,9 @@
 ﻿using ControleBolsa.DAL;
 using ControleBolsa.Models;
 using System;
+using System.Collections;
 using System.Data.Entity;
+using System.Linq;
 
 namespace ControleBolsa.Console
 {
@@ -29,7 +31,27 @@ namespace ControleBolsa.Console
 
         private static void InicializarBanco()
         {
+            var path = GetDataBaseRelativePath();
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+
             Database.SetInitializer(new MyInitializer());
+        }
+
+        private static string GetDataBaseRelativePath()
+        {
+            var path = AppDomain.CurrentDomain.BaseDirectory.Split('\\');
+
+            Stack pilha = new Stack(path);
+
+            Enumerable.Range(0, 4).ToList().ForEach(x => pilha.Pop());
+
+            pilha.Push("ControleBolsa.UI.Web");
+            pilha.Push("App_Data");
+
+            var arr = pilha.ToArray();
+            Array.Reverse(arr);
+
+            return string.Join("\\", arr);
         }
 
         class MyInitializer : DropCreateDatabaseIfModelChanges<Context>
